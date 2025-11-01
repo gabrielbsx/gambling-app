90bf2f95c8364245eaa9b0c5fe39064b395bfd85
Gabriel Barbosa <gabrielturing@gmail.com>
2025-11-01 19:10:30 -0300

    feat: update migration script to include commerce migration

--- src/main/bootstrap/migration.ts
+++ src/main/bootstrap/migration.ts
@@ -1,3 +1,5 @@
+import { commerceMigration } from "@/modules/commerce/infrastructure/database/kysely/config/migrator.js";
 import { identityMigration } from "@/modules/identity/infrastructure/database/kysely/config/migrator.js";
 
-await identityMigration()
+await identityMigration();
+await commerceMigration();
